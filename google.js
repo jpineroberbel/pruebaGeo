0f8a3b8eaea0520d11d70d5fdb33e33e6171178a
Jose Piñero <jpineroberbel@gmail.com>
2023-10-03 09:41:53 +0200

    Add files via upload

---
+++ google.js
@@ -0,0 +1,29 @@
+// Initialize and add the map
+let map;
+
+async function initMap(pos) {
+
+
+  // The location of Uluru
+  const position = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+navigator.geolocation.getCurrentPosition(initMap);
